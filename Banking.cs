db47dc865b213db28c4b1265ed6f5e98ff412c40
anirbanghoshal21 <111297775+anirbanghoshal21@users.noreply.github.com>
2022-09-07 00:17:17 +0530

    prob 7

---
+++ Banking.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace Person
+{
+    public static class Banking
+    {
+
+        public static void deposit(this System.Object s, int bal)
+        {
+            Console.WriteLine("Enter the amount to be deposited");
+            int amt = int.Parse(Console.ReadLine());
+            if (amt > 0)
+            {
+                bal = bal - amt;
+                Console.WriteLine("Deposit successfully");
+            }
+            else
+            {
+                Console.WriteLine(" 0 can't be a amount");
+            }
+        }
+        public void TotNoOfDaysDeposit(object NoOfDays)
+        {
+            Console.WriteLine($" NoOfdays: {NoOfDays}");
+        }
+    }
+}
+    
+
