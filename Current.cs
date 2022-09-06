019989933a71177d453a471fab9f4e7d69bd169a
anirbanghoshal21 <111297775+anirbanghoshal21@users.noreply.github.com>
2022-09-06 21:37:01 +0530

    Add files via upload

---
+++ Current.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Person
+{
+    public class Current : Ibank
+    {
+        public Current()
+        {
+            Console.WriteLine("Opening of new current account");
+        }
+        public void CalInterest()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void PrintBalance()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Saving.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Person
+{
+    public class Saving : Ibank
+    {
+        public Saving()
+        {
+            Console.WriteLine("Opening of new saving account");
+        }
+        public void CalInterest()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void PrintBalance()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
