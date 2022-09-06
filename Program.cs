43a13fc279099f815e6733fbf73695eb4e81bfe1
anirbanghoshal21 <111297775+anirbanghoshal21@users.noreply.github.com>
2022-09-07 00:02:57 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_oops
+{
+    interface Ibank
+    {
+        void Printbalance();
+
+        void CalculateInterest();
+    }
+
+    static void Main(string[] args)
+    {
+        Manager m = new Manager();
+        Employee e = new Employee();
+        Clerk c = new Clerk();
+
+        Console.WriteLine(" HDFC Bank");
+    }
+}
