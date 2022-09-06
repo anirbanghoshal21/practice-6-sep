eddafa0b556567f282175443dd3ba3b8580fc75d
anirbanghoshal21 <111297775+anirbanghoshal21@users.noreply.github.com>
2022-09-06 23:20:16 +0530

    Add files via upload

---
+++ Banking.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Person
+{
+    public static class Banking
+    {
+        public static void withdraw(this System.Object s,int bal)
+        {
+            Console.WriteLine("Enter the amount to be withdrawn");
+            int amt = int.Parse(Console.ReadLine());
+            if (bal > amt)
+            {
+                bal = bal - amt;
+                Console.WriteLine("Amount withdraw successfully");
+            }
+            else
+            {
+                Console.WriteLine("Insufficent balance");
+            }
+        }
+    }
+}
