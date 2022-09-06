e208855ad02af69a5da9952f782bb995ed56e01e
anirbanghoshal21 <111297775+anirbanghoshal21@users.noreply.github.com>
2022-09-06 21:34:31 +0530

    Add files via upload

---
+++ Ibank.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Person
+{
+    public interface Ibank
+    {
+        public static int bal = 0;
+
+        public int balance
+        {
+            get { return bal; }
+            set { bal = value; }
+        }
+        public void Calculateinterest()
+        {
+           
+        }
+
+    }
+}
+
