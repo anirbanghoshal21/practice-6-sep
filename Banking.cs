bf784e94d5012382dd9e9c24f5526d9571989591
anirbanghoshal21 <111297775+anirbanghoshal21@users.noreply.github.com>
2022-09-06 23:29:30 +0530

    prob 6

--- Banking.cs
+++ Banking.cs
@@ -8,19 +8,21 @@ namespace Person
 {
     public static class Banking
     {
-        public static void withdraw(this System.Object s,int bal)
-        {
-            Console.WriteLine("Enter the amount to be withdrawn");
-            int amt = int.Parse(Console.ReadLine());
-            if (bal > amt)
+      
+            public static void deposit(this System.Object s, int bal)
             {
-                bal = bal - amt;
-                Console.WriteLine("Amount withdraw successfully");
-            }
-            else
-            {
-                Console.WriteLine("Insufficent balance");
+                Console.WriteLine("Enter the amount to be deposited");
+                int amt = int.Parse(Console.ReadLine());
+                if (amt > 0)
+                {
+                    bal = bal - amt;
+                    Console.WriteLine("Deposit successfully");
+                }
+                else
+                {
+                    Console.WriteLine(" 0 can't be a amount");
+                }
             }
         }
-    }
-}
+    } 
+
